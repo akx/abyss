db05ba5b2993742a3acc03691fa30c8137974455
Aarni Koskela <akx@iki.fi>
2017-03-20 14:22:33 +0200

    Add Django middleware

---
+++ abyss/django.py
@@ -0,0 +1,20 @@
+import datetime
+import os
+
+from abyss.simple import profiling
+
+
+def abyss_middleware(get_response):
+    def middleware(request):
+        timestamp = datetime.datetime.now().isoformat().replace(':', '-')
+        path_stamp = request.path.strip('/').replace('/', '-')
+        filename = 'abyss-django/%s_%s.tracing' % (path_stamp, timestamp)
+        dir = os.path.dirname(filename)
+        if not os.path.isdir(dir):
+            os.makedirs(dir)
+        with profiling(filename):
+            response = get_response(request)
+            if hasattr(response, 'render') and callable(response.render):
+                response.render()
+            return response
+    return middleware
