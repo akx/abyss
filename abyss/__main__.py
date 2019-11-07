3ae4d62d4434f6833d19f2ba307b018c2f2c517e
Aarni Koskela <akx@iki.fi>
2019-11-07 12:11:34 +0200

    Add __main__

---
+++ abyss/__main__.py
@@ -0,0 +1,14 @@
+import sys
+import runpy
+from abyss.simple import profiling
+
+
+def main():
+    sys.argv = sys.argv[1:]
+    print(sys.argv)
+    with profiling(interval=0.001):
+        runpy.run_path(sys.argv[0], run_name="__main__")
+
+
+if __name__ == '__main__':
+    main()
